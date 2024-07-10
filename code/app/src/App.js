adee8936ee481d57c5dcc761fedf2663f87e9e42
Jan Schulz <jan.schulz@cileria.com>
2024-07-10 08:53:21 +0200

    added some fetch

--- code/app/src/App.js
+++ code/app/src/App.js
@@ -3,10 +3,22 @@ import Button from "@mui/material/Button";
 const theme = createTheme();
 
 function App() {
+  const loadData = async () => {
+    try {
+      const response = await fetch("http://localhost:3002/aiprompts");
+      const aiPrompts = await response.json();
+      console.log(aiPrompts);
+    } catch (e) {
+      alert(`Could not load AI Prompts`);
+    }
+  };
+
   return (
     <div>
       <h1>Hallo World</h1>
-      <Button variant="text">Click Me!</Button>
+      <Button variant="text" onClick={loadData}>
+        Click Me!
+      </Button>
     </div>
   );
 }
